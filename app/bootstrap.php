16af9f1a007291f1e7af08de4215055c65359636
arkafreak <dutta.arkapravo11@gmail.com>
2024-11-05 10:24:50 +0530

    .

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once 'config/config.php';
-
+require_once '../vendor/autoload.php';
 
 spl_autoload_register(function ($className) {
     require_once 'libraries/' . $className . '.php';
